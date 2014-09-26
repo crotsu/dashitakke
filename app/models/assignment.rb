24c34d3ba0fc6536110778fecda5fd2e3ff7652b
Shinichi OEDA <crotsu@gmail.com>
2014-09-26 10:17:21 +0900

    達成度を追加
    model/assignment.rbにメソッドをいくつか追加した．

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -3,6 +3,7 @@ class Assignment < ActiveRecord::Base
   belongs_to :user
   has_many :answers, dependent: :destroy
 
+  # 未完了の問題
   def undone_answers
     undone = Array.new
     self.answers.each do |answer|
@@ -12,4 +13,30 @@ class Assignment < ActiveRecord::Base
     end
     return undone
   end
+
+  # 完了している問題
+  def done_answers
+    done = Array.new
+    self.answers.each do |answer|
+      if answer.status == "DONE"
+        done.push answer
+      end
+    end
+    return done
+  end
+
+  # 問題の数
+  def count_questions
+    return self.paper.count_questions
+  end
+
+  # 達成度
+  def progress
+    if (self.count_questions==0)
+      return 0
+    else
+      return (self.done_answers.length.to_f / self.count_questions * 100).to_i
+    end
+  end
+
 end
--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,4 +1,8 @@
 class Paper < ActiveRecord::Base
 	has_many :questions, dependent: :destroy
 	has_many :answers, dependent: :destroy
+
+  def count_questions
+    return self.questions.length
+  end
 end
