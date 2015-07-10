b97daf6033b3a8d67bc9388a9c11229fd14a5c70
Kouta ASAI <scene.4913@gmail.com>
2015-07-10 13:47:20 +0900

    アップロードしたソースファイルを見るときに、強制的に文字コードをUTF-8に変換するようにした。

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,3 +1,5 @@
+require 'kconv'
+
 class Source < ActiveRecord::Base
   belongs_to :answer
 
@@ -6,14 +8,18 @@ class Source < ActiveRecord::Base
   save_dir_path = "#{Rails.root}/public/source_code/" + "j" + number.to_s[0..1] + "/j" + number.to_s.delete("-") + "/"
 
   has_attached_file :avatar, path: save_dir_path + ":filename"
-#  validates_attachment :avatar, content_type: { content_type: ["application/octet-stream", "text/plain"] }
 
+  # avatarは必須
   validates_attachment_presence :avatar
+  # *.cしか受け付けない
   validates_attachment_file_name :avatar, :matches => [/.c\Z/]
+  # 100kB未満のファイルしか受け付けない
+  validates_attachment_size :avatar, :less_than => 100.kilobytes
 
   def getSourcefile path
     f = open(path, "r")
-    srcFile = f.read.scrub('?')
+    # 文字コードをUTF-8に変換する。それでも不正なバイト文字が含まれていたら'?'で置換
+    srcFile = f.read.toutf8.scrub('?')
     f.close
     return srcFile
   end
