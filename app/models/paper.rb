a0a4012f1b1f2f39fb1eab8161b934613cb700e4
Kouta ASAI <scene.4913@gmail.com>
2015-07-14 14:46:03 +0900

    課題番号を変更できるように戻した。バリデーションもかけてある。

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -2,6 +2,9 @@ class Paper < ActiveRecord::Base
 	has_many :questions, dependent: :destroy
 	has_many :assignments, dependent: :destroy
 
+	validates :index, numericality: { greater_than: 0 }
+	validates :index, uniqueness: true
+
   def count_questions
     return self.questions.length
   end
